16ec6bf0b6272df85fd2b67e77feba20b78f7118
ossan <ivan.pesenti.dev@gmail.com>
2024-07-11 12:47:14 +0200

    review 2024-07-11

--- Backend/routes/users.routes.js
+++ Backend/routes/users.routes.js
@@ -9,9 +9,6 @@ const {
 } = require('../controllers/users.controllers')
 
 
-// FIXME: these routes are wrong.
-// suggested readings: https://www.freecodecamp.org/news/rest-api-best-practices-rest-endpoint-design-examples/
-// https://stackoverflow.blog/2020/03/02/best-practices-for-rest-api-design/
 router.post('/users', addUser)
 router.put('/users/:id', modifyUser)
 router.put('/users/:id/password', modifyUserPassword)
